a71ca53794f8b59d4d768be8097c7cd4a2c89535
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-07-09 11:49:50 -0500

    new

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your Lastname? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your Grade Percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int intPercentage = int.Parse(gradePercentage);
+
+        string calification = "";
+
+        if (intPercentage >= 90)
+        {
+            calification = "A";
+        }
+        else if (intPercentage >= 80)
+        {
+            calification = "B";
+        }
+        else if (intPercentage >= 70)
+        {
+            calification = "C";
+        }
+        else if (intPercentage >= 60)
+        {
+            calification = "D";
+        }
+        else
+        {
+            calification = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {calification}");
+
     }
 }
